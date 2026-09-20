d65a06e8ab7f27126e29b0796cddf38cf30b1351
coder123454321 <115930973+coder123454321@users.noreply.github.com>
2026-09-20 23:47:39 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
 //your JS code here. If required.
+const a=document.getElementById("counter")
+const btn=document.getElementById("incrementBtn");
+	let count=0;
+function Incriment() {
+	count++;
+	a.innerHTML=count;
+	alert(count);
+}
